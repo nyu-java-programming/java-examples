b4c39fb59f7cabae2a45ccc022e0f7694cb5a435
bloombar <2209098+bloombar@users.noreply.github.com>
2018-11-06 16:53:32 -0500

    formatting

--- Inheritance/src/edu/nyu/cs/fb1258/inheritance_examples/people_and_students/TestPersons.java
+++ Inheritance/src/edu/nyu/cs/fb1258/inheritance_examples/people_and_students/TestPersons.java
@@ -17,6 +17,8 @@ public class TestPersons {
 		// just because we can, let's store a NYUStudent object into a Person-typed reference... polymorphism!
 		Person z = new NYUStudent("Jennifer Rong Do", 19, 'F'); // instantiate using full args constructor, store in a Person-typed variable!
 		
+		// and just because we can, let's put all 
+		
 		// output the string representations of all three
 		System.out.printf("The three students are:\n* %s\n* %s\n* %s\n", x, y, z);
 
--- Processing/src/edu/nyu/cs/fb1258/processing_examples/basics/SmileyBouncer.java
+++ Processing/src/edu/nyu/cs/fb1258/processing_examples/basics/SmileyBouncer.java
@@ -12,11 +12,11 @@ import processing.core.*;
 public class SmileyBouncer extends PApplet {
 
 	//instance properties	
-	
+
 	//basic window size settings
 	private final int h = 600;
 	private final int w = 400;
-	
+
 	//the starting settings of the circle
 	private int x = 200;
 	private int y = 200;
@@ -38,63 +38,63 @@ public class SmileyBouncer extends PApplet {
 		//must call PApplet's static main method, and supply the full package and class name of this class as an argument
 		PApplet.main("edu.nyu.cs.fb1258.processing_examples.basics.SmileyBouncer");
 	}
-	
+
 	//application-wide settings
 	public void settings() {
-	  this.size(this.w, this.h);
-	  
-	  //load up our smiley face icon
-	  this.smiley = this.loadImage("src/smiley.png");
+		this.size(this.w, this.h);
 	}
 
 	//settings for the first frame
 	public void setup() {
-	
-	  this.background(255, 255, 255); //white background
-	  
-	  //set up the fill and stroke colors
-	  this.fill(0, 0, 255); //rgb
-	  this.stroke(255, 0, 0); //rgb
+
+		//load up our smiley face icon
+		this.smiley = this.loadImage("src/smiley.png");
+
+		this.background(255, 255, 255); //white background
+
+		//set up the fill and stroke colors
+		this.fill(0, 0, 255); //rgb
+		this.stroke(255, 0, 0); //rgb
 	}
 
 	//frame-by-frame animation settings
 	public void draw() {
-		  this.background(255, 255, 255); //white background
+		this.background(255, 255, 255); //white background
 
-	  //draw the cursor image
-	  this.imageMode(PConstants.CENTER); //make the x, y coordinate of the image center
-	  this.image(this.smiley, this.mouseX, this.mouseY);  //draw the smiley image at the mouse's position
-	  
-	  //draw the ellipse
-	  this.ellipse(this.x, this.y, this.circle_radius*2,  this.circle_radius*2); //draw the ellipse
+		//draw the cursor image
+		this.imageMode(PConstants.CENTER); //make the x, y coordinate of the image center
+		this.image(this.smiley, this.mouseX, this.mouseY);  //draw the smiley image at the mouse's position
 
-	  //adjust the position of the ellipse
-	  this.x = this.x + this.speedX; //move in the x  direction
-	  this.y = this.y + this.speedY; //move in the y  direction
-	  
-	  //check the bounds of the x position  of the ellipse
-	  if (this.x >= this.width - this.circle_radius || this.x  <= 0 + this.circle_radius) {
-	   this.speedX = this.speedX * -1;
-	  } 
+		//draw the ellipse
+		this.ellipse(this.x, this.y, this.circle_radius*2,  this.circle_radius*2); //draw the ellipse
 
-	  //check the bounds of the y position  of the ellipse
-	  if (this.y >= this.height - this.circle_radius || this.y  <= 0 + this.circle_radius) {
-	   this.speedY = this.speedY * -1;
-	  } 
-	  
-	  //check for collision between circle  and smiley
-	  if (this.isCollision == false && (abs( this.mouseX - this.x) < 50 && abs(this.mouseY - this.y) <  50)) {
-	    //if we have a collision, reverse  the direction of the ball
-	    this.speedX = this.speedX * -1;
-	    this.speedY = this.speedY * -1;
-	    //remember that we have a collsion  so we don't look for more collisions
-	    this.isCollision = true;
-	  }
-	  else if (abs(this.mouseX - this.x) > 50 && abs( this.mouseY - this.y) > 50) {
-	      //if we had a collsion, but we  don't have one any more, reset  the flag
-	      this.isCollision = false;
-	  }
-	    
+		//adjust the position of the ellipse
+		this.x = this.x + this.speedX; //move in the x  direction
+		this.y = this.y + this.speedY; //move in the y  direction
+
+		//check the bounds of the x position  of the ellipse
+		if (this.x >= this.width - this.circle_radius || this.x  <= 0 + this.circle_radius) {
+			this.speedX = this.speedX * -1;
+		} 
+
+		//check the bounds of the y position  of the ellipse
+		if (this.y >= this.height - this.circle_radius || this.y  <= 0 + this.circle_radius) {
+			this.speedY = this.speedY * -1;
+		} 
+
+		//check for collision between circle  and smiley
+		if (this.isCollision == false && (abs( this.mouseX - this.x) < 50 && abs(this.mouseY - this.y) <  50)) {
+			//if we have a collision, reverse  the direction of the ball
+			this.speedX = this.speedX * -1;
+			this.speedY = this.speedY * -1;
+			//remember that we have a collsion  so we don't look for more collisions
+			this.isCollision = true;
+		}
+		else if (abs(this.mouseX - this.x) > 50 && abs( this.mouseY - this.y) > 50) {
+			//if we had a collsion, but we  don't have one any more, reset  the flag
+			this.isCollision = false;
+		}
+
 	}
 
 }
