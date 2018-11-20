79b1cf7bb0511e607fc12cca97016110160f8570
bloombar <2209098+bloombar@users.noreply.github.com>
2018-11-20 12:05:22 -0500

    updating comment

--- Interfaces/src/edu/nyu/cs/fb1258/thanksgiving_hunger/Revenge.java
+++ Interfaces/src/edu/nyu/cs/fb1258/thanksgiving_hunger/Revenge.java
@@ -3,6 +3,7 @@ package edu.nyu.cs.fb1258.thanksgiving_hunger;
 /**
  * A class based on the phrase, "Revenge is a dish best served cold."
  * Implements the Servable interface, which means it must implement 'serve' and 'isReadyToServe' methods.
+ * Revenge is quite different from a Meal, although both implement the same behaviors defined in the Servable interface.
  * @author Foo Barstein
  * @version 1
  *
