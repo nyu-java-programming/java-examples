1c11ec496d0cc853ae4a58e882691456eb1681d3
bloombar <2209098+bloombar@users.noreply.github.com>
2020-03-02 20:02:37 -0500

    better version

--- Multi-Dimensional Arrays/src/edu/nyu/cs/fb1258/array_examples/TicTacToe.java
+++ Multi-Dimensional Arrays/src/edu/nyu/cs/fb1258/array_examples/TicTacToe.java
@@ -1,227 +1,139 @@
 package edu.nyu.cs.fb1258.array_examples;
 
-import java.util.Random;
+import java.util.Arrays;
 import java.util.Scanner;
 
 /**
- * A virtual game of Tic Tac Toe, played by two humans sitting at the same computer.
+ * Two-player Tic Tac Toe on the command line.
  * @author Foo Barstein
- * @version 22
- * 
+ * @version 2
  */
 public class TicTacToe {
 	
-	// class variables that represent each state a given position on the board could be in
-	public final static char X = 'X';
-	public final static char O = 'O';
-	public final static char EMPTY = '-';
-	
-	public static void main(String[] args) {
-		// get a new blank board
-		char[][] board = getBoard();
-		
-		// a few flags to maintain state
-		boolean isWon = false; // whether the game has been won or not
-		boolean isFull = false; // whether the board is full or not
-
-		// the game starts with player X's turn
-		char currentPlayer = X;
-		
-		// iterate until game is over
-		boolean gameOver = false;
-		while (!gameOver) {
-			
-			// print out the board
-			outputBoard(board);
-			
-			// ask the user where they want to go.. returns an int array with the desired row, column
-			int[] move = getUsersMove(currentPlayer);
-			int row = move[0]; // the row where the user desires to go
-			int col = move[1]; // the column where the user desires to go
-
-			// check whether the user's desired move is legal
-			if (isLegalMove(row, col, board)) {
-				// if so, make the move!
-				makeMove(currentPlayer, board, row, col);
-			}
-			else {
-				// the move is invalid... inform the user and try again
-				System.out.println("Sorry, that position is already filled.  Please try again...");
-				continue; // start the next iteration of the loop without switching players
-			}
-			
-			// check whether there is a win or a draw
-			isWon = isWin(board, currentPlayer);
-			isFull = isBoardFull(board);
-			gameOver = isWon || isFull;
-			
-			// output a nice message if someone wins or the board is full and it's a tie
-			if (isWon) {
-				System.out.printf("\n%s WINS!!!\n", currentPlayer);
-			}
-			else if (isFull) {
-				System.out.println("\nTIE!!!");
-			}
-			
-			// switch players before next iteration
-            currentPlayer = (currentPlayer == X) ? O : X;
-			
-		}
-
-		// the game is over once the loop exits
-		System.out.println("Game over!");
-		
-	}
 	/**
-	 * Creates an empty board for a new game of Tic Tac Toe
-	 * @return a two-dimensional char array representing an empty board
+	 * Print out the game board
+	 * @param board The game board data
 	 */
-	public static char[][] getBoard() {
+	public static void printBoard(String[][] board) {
+		// print the first line across the top
+		System.out.println("-------------");
 
-		// the board is represented as a char array of two-dimensions ... thought of as rows and columns
-		char[][] board = {
-				//col1,  col2,  col3
-				{EMPTY, EMPTY, EMPTY}, // row1
-				{EMPTY, EMPTY, EMPTY}, // row2
-				{EMPTY, EMPTY, EMPTY} // row3
-		};
-		
-		return board;
-		
-	}
-
-	/**
-	 * Outputs the state of the board
-	 * @param board The current board two-dimensional array
-	 */
-	public static void outputBoard(char[][] board) {
-		
-		/// print a line break above the board
-		System.out.print("\n");
-
-		// print out the board (the hard way)
-		System.out.println("The current state of the board: ");
-		for (int rowNum=0; rowNum<board.length; rowNum++) {
-			// get a single row 
-			char[] thisRow = board[rowNum];
+		for (String[] row : board) {
+			// print the first pipe for each line
+			System.out.print("| ");
 			
-			// loop through the columns within this row
-			for (int colNum=0; colNum<thisRow.length; colNum++) {
-				// print out what's at ths position within 4 spaces
-				System.out.printf("%4s", thisRow[colNum]);
+//			System.out.println( Arrays.toString(row) );
+			
+			for (String col : row) {
 				
+				// if this spot is blank, print a space
+				col = (col.trim().equals("")) ? " " : col;
+				
+				// print the value in this column, plus a pipe
+				System.out.print(col + " | ");
 			}
 			
-			//  print a line break between rows
-			System.out.print("\n");
+			// print a line break at the end of each row
+			System.out.println("");
+			
+			// print a row separator after each row
+			System.out.println("-------------");
 			
 		}
-		
-		/// print a line break after all rows have been printed
-		System.out.print("\n");
-		
-		// there is a far simpler syntactical way to do the same kind of nested loop (a.k.a some syntactic sugar)
-		// print out the board with foreach style loop
-//		for (char[] row : board) {
-//			for (char col : row) {
-//				System.out.println(col);
-//			}
-//		}
-		
-	}
-
-	/**
-	 * Get the current player's move
-	 * @param player The player whose turn it is
-	 * @return int array with the player's desired row and column for their move
-	 */
-	public static int[] getUsersMove(char player) {
-		// generate a random example move
-		Random randy = new Random();
-		int exampleRow = randy.nextInt(3) + 1; // a random int between 1-3
-		int exampleCol = randy.nextInt(3) + 1; // a random int between 1-3
-		
-		// ask the user where they would like to go
-		System.out.printf("Player %s ... where would you like to go? (e.g. '%d,%d' for row %d column %d)\n", player, exampleRow, exampleCol, exampleRow, exampleCol);
-		Scanner scanly = new Scanner(System.in);
-		String response = scanly.nextLine(); // get user's response
-
-		// split the user's response to get the row and column separated
-		String[] move = response.split(",");
-
-		//clean up the array data to remove any extra whitespace the user may have entered
-		for (int i=0; i<move.length; i++) {
-			move[i] = move[i].trim(); //get rid of any whitespace
-		}		
-
-		// extract the row and column data as integers from this string
-		int row = Integer.parseInt(move[0]) - 1; // subtract 1 from what the user entered, since they entered it starting from 1, whereas array indices start from 0
-		int col = Integer.parseInt(move[1]) - 1;
-		
-		// return int array with the row and column where the user wants to move
-		int[] moveAsInts = {row, col};
-		return moveAsInts;
-	}
-
-
-	/**
-	 * Checks whether a given move is legal or not.  A legal move is any move within bounds into an empty space.
-	 * @param row The row index of the desired move
-	 * @param col The column index of the desired move.
-	 * @param board the board upon which this move will be played
-	 * @return True if the move is legal, false otherwise.
-	 */
-	private static boolean isLegalMove(int row, int col, char[][] board) {
-		// a legal move is a move into any empty spot on the board
-		boolean isLegalMove = board[row][col] == EMPTY; // true if the position on the board is empty, false otherwise
-		return isLegalMove;
-	}
-
-	/**
-	 * Checks whether the game board is full, meaning there are no empty spots left.
-	 * @param board the board to check
-	 * @return true if the board is full, false otherwise
-	 */
-	public static boolean isBoardFull(char[][] board) {
-		// let's start by assuming the board is full
-		boolean isBoardFull = true;
-		
-		//iterate through each row... check out how we exit a nested loop with the 'break to label' technique
-	row_loop: // a label!
-		for (char[] row : board) {
-			// iterate through each column in this row
-			for (char col : row) {
-				// check whether it's empty
-				if (col == EMPTY) {
-					isBoardFull = false; // we now know the board is not totally full, so no point continuing to iterate
-					break row_loop; // break out of the row loop!
-				}
-			}				
-		}
-		
-		return isBoardFull;
-	}
-
-	/**
-	 * Make the move on the board for the selected player
-	 * @param player The player whose move it is
-	 * @param board The board upon which to make the move
-	 * @param row The row where they want to put their character
-	 * @param col The column where they want to put their character
-	 */
-	public static void makeMove(char player, char[][] board, int row, int col) {
-		// update the board at this row and column
-		board[row][col] = player;		
 	}
 	
-
 	/**
-	 * Checks for a win by one of the players.
-	 * @param board The board upon which the players are playing
-	 * @param player The player for whom to check for a win
-	 * @return True if this player won.  False otherwise
+	 * Check whether a particular board spot is empty
+	 * @param board The board
+	 * @param row The row of interest
+	 * @param col The column of interest
+	 * @return True if the board spot is empty, false otherwise.
 	 */
-	public static boolean isWin(char[][] board, char player) {
+	public static boolean moveIsValid(String[][] board, int row, int col) {
+		// return whether empty or not
+		return board[row][col].trim().equals("");
+	}
+	
+	/**
+	 * Ask the player where they want to move on the board.
+	 * @param scn A Scanner object to use to solicit user input
+	 * @param player The player to ask to move.
+	 * @return The response from the user.
+	 */
+	public static String solicitMoveFromPlayer(Scanner scn, String player) {
+		// ask the user where they want to go
+		System.out.println(player + "'s turn!!!");
+		System.out.println("Where do you want to move? (e.g. 1,2) ");
+		// assume the user is typing something like, "1,2"
+		String response = scn.nextLine();
+		return response;		
+	}
+	
+	/**
+	 * Convert a string in the format, "1,2" to an integer array of row, column coordinates for a position on the game board.
+	 * @param response The string we need to parse
+	 * @return An int array in the format {row, col}
+	 */
+	public static int[] getCoordsFromResponse(String response) {
+		// assuming the response is something like "1,2", we split by comma
+		String[] strCoords = response.split(",");			
+		
+		// remove any whitespace from each part
+		strCoords[0] = strCoords[0].trim();
+		strCoords[1] = strCoords[1].trim();
+		
+		// convert each part to integer and subtract one to get the corresponding array index
+		int row = Integer.parseInt( strCoords[0] ) - 1;
+		int col = Integer.parseInt( strCoords[1] ) - 1;
+//		System.out.println(row + ":" + col);
+		
+		// convert into arraay
+		int[] intCoords = { row, col };
+		
+		return intCoords;
+		
+	}
+	
+	/**
+	 * Place a player's mark on the board
+	 * @param board The board data
+	 * @param player The player - X or O
+	 * @param row - the index of the row in which to place the player's mark
+	 * @param col - the index of the column in which to place the player's mark
+	 */
+	public static void makeMove(String[][] board, String player, int row, int col) {
+		// place this player's character on the board at the correct position
+		board[row][col] = player;
+	}
+	
+	/**
+	 * Check whether the game board is full
+	 * @param board The game board data
+	 * @return True if full, false otherwise.
+	 */
+	public static boolean isBoardFull(String[][] board) {
+		// loop through each row
+		for (String[] row : board) {
+			// loop through each column
+			for (String col : row) {
+				// if any column is empty, the board is not full
+				if (col.trim().equals("")) {
+					// not full!
+					return false;
+				}
+			}
+		}
+		// full!
+		return true;
+	}
+	
+	/**
+	 * Check whether a given player has won the game.
+	 * @param board The game board data
+	 * @param player The player to check for a win
+	 * @return True if won, false otherwise
+	 */
+	public static boolean isWin(String[][] board, String player) {
 		boolean isWin = false;
 		
 		// Try it yourself!... these tasks need to be implemented:
@@ -230,6 +142,69 @@ public class TicTacToe {
 		// check for both diagonal wins
 		
 		return isWin;
+		
 	}
+
+	public static void main(String[] args) {
+		
+		// our game board with blank strings to start
+		String[][] board = {
+				{"", "", ""},
+				{"", "", ""},
+				{"", "", ""}
+		};
+		
+		// X goes first
+		String player = "X";
+
+		// we will need user input
+		Scanner scn = new Scanner(System.in);
+
+		// infinite loop... for now
+		boolean keepPlaying = true;
+		while (keepPlaying) {
+
+			// print out the board
+			printBoard(board);
+			
+			// get the current user's move
+			String response = solicitMoveFromPlayer(scn, player);
+			
+			// get the integer coordinates from the user's response
+			int[] coords = getCoordsFromResponse(response);
+			
+			// get the row and column indices from the response
+			int row = coords[0];
+			int col = coords[1];
+			
+			// check whether the move is valid
+			if (moveIsValid(board, row, col)) {
+				// make the move if so!
+				makeMove(board, player, row, col);
+			}
+			else {
+				// the move is invalid
+				System.out.println("Sorry buster, not a valid move!");
+				continue; // do another iteration of this loop
+			}
+			
+			// check whether the game is over
+			if (isBoardFull(board) || isWin(board, player) ) {
+				System.out.println("Game over!");
+				keepPlaying = false;
+			}
+			else {
+				// switch players
+				player = (player.equals("X")) ? "O" : "X";
+			}
+			
+			
+			
+		} // while
+		
+		// close the scanner
+		scn.close();
+		
+	} // main
 
 }
